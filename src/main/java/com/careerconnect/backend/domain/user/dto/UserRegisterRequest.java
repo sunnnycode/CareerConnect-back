db4bc204790e7db7edb94a07d8eb696b0185f465
Sunny Lee <86199470+sunnnycode@users.noreply.github.com>
2024-08-23 13:51:55 +0900

    validation ckeck 및 error 처리

--- src/main/java/com/careerconnect/backend/domain/user/dto/UserRegisterRequest.java
+++ src/main/java/com/careerconnect/backend/domain/user/dto/UserRegisterRequest.java
@@ -1,6 +1,5 @@
 package com.careerconnect.backend.domain.user.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,13 +12,13 @@ import lombok.NoArgsConstructor;
 public class UserRegisterRequest {
 
 
-    @NotBlank
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
     private String userId;
 
-    @NotBlank
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String passwordHash;
 
 
